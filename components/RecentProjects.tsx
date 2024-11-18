d0dde25aa9edaaa5d9bb24f137b6558be9a4c564
NotAnonymousUser <mubeen.javed2000@gmail.com>
2024-11-18 15:23:52 +0500

    Recent Projects now Complete

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -6,7 +6,7 @@ import { FaLocationArrow } from "react-icons/fa6";
 
 const RecentProjects = () => {
   return (
-    <div className="py-20">
+    <div className="py-20" id="projects">
       <h1 className="text-5xl font-extrabold text-center">
         A small selection of{" "}
         <span className="text-purple">recent projects</span>
