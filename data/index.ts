92e364767e85654a5c24ddaa54b9fa7b74b4f89c
NotAnonymousUser <mubeen.javed2000@gmail.com>
2024-11-19 01:45:16 +0500

    Completed , finishing touches maybe needed later

--- data/index.ts
+++ data/index.ts
@@ -205,13 +205,11 @@ export const socialMedia = [
   {
     id: 1,
     img: "/git.svg",
-  },
-  {
-    id: 2,
-    img: "/twit.svg",
+    url: "https://github.com/NotAnonymousUser",
   },
   {
     id: 3,
     img: "/link.svg",
+    url: "https://www.linkedin.com/in/mubeen-javaid/",
   },
 ];
