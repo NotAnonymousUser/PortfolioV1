4c34a2d03db5cdc39e3b8f06bda1ba2e61aac850
NotAnonymousUser <mubeen.javed2000@gmail.com>
2024-11-18 15:16:30 +0500

    Recent Projects is now ready

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Mubeen's Portfolio",
+  title: "Muhammad Mubeen's Portfolio",
   description: "Modern & Minimalisit Portfolio",
 };
 
